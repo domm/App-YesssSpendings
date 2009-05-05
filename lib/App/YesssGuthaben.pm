8937c5db7bf7586d0b5fa6cfd135b09ddb245fd0
Thomas Klausner <domm@cpan.org>
2009-05-05 15:28:11 +0200

    initial commit

---
+++ lib/App/YesssGuthaben.pm
@@ -0,0 +1,34 @@
+package App::YesssGuthaben;
+
+use strict;
+use warnings;
+use 5.010;
+use version; our $VERSION = version->new('0.01');
+
+1;
+__END__
+
+=head1 NAME
+
+App::YesssGuthaben -
+
+=head1 SYNOPSIS
+
+  use App::YesssGuthaben;
+
+=head1 DESCRIPTION
+
+App::YesssGuthaben is
+
+=head1 AUTHOR
+
+Thomas Klausner E<lt>domm {at} cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
