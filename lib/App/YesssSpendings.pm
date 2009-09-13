6d514ea417bc2af1fbecaca4baf33b42a397461d
Thomas Klausner <domm@cpan.org>
2009-09-13 09:34:16 +0200

    adapted to new YESSS site

--- lib/App/YesssSpendings.pm
+++ lib/App/YesssSpendings.pm
@@ -17,9 +17,11 @@ has msisdn => (is=>'ro',isa=>'Str',required=>1);
 has passwd => (is=>'ro',isa=>'Str',required=>1);
 has mech   => (is=>'rw',isa=>'WWW::Mechanize',default=>sub { WWW::Mechanize->new });
 has yesss_login  => (is=>'ro',isa=>'Str',default=>'http://www.yesss.at/kontomanager.php');
-has yesss_bookings=> (is=>'ro',isa=>'Str',default=>'https://www.yesss.at/kontomanager/wertkarte_gespraeche.php?page=');
+#has yesss_bookings=> (is=>'ro',isa=>'Str',default=>'https://www.yesss.at/kontomanager/wertkarte_gespraeche.php?page=');
+has yesss_bookings=> (is=>'rw',isa=>'Str',default=>'https://www.yesss.at/kontomanager/vertrag_gespraeche.php?');
 has verbose => (is=>'rw',isa=>'Bool',default=>0);
 has budget => (is=>'ro',isa=>'Str');
+has session_id=>(is=>'rw',isa=>'Str');
 
 no Moose;
 __PACKAGE__->meta->make_immutable;
@@ -36,10 +38,13 @@ sub login {
     $self->mech->get($self->yesss_login);
     $self->mech->submit_form(
         with_fields=>{
-            rufnummer=>$self->msisdn,
-            passwort=>$self->passwd,
+            login_rufnummer=>$self->msisdn,
+            login_passwort=>$self->passwd,
         }
     );
+    if ($self->mech->uri->query =~ /(PHPSESSID=[\dabcdef]+)/) {
+        $self->yesss_bookings($self->yesss_bookings . $1 .'&');
+    }
 }
 
 sub get_this_months_bookings {
@@ -48,15 +53,18 @@ sub get_this_months_bookings {
     my $last_day_of_prev_month = DateTime->now->truncate('to'=>'month')->subtract('days'=>1)->dmy('.');
 
     my $sum;
+    my %types;
     PAGE: foreach my $page (1 .. 10) {
-        my $table_extract = HTML::TableExtract->new(headers => ['Datum/Uhrzeit:',qw(Nummer: Dauer: Kosten: Art: Verrechnung:)]);
-        $self->mech->get($self->yesss_bookings . $page);
+        my $table_extract = HTML::TableExtract->new();#headers => ['Datum/Uhrzeit:',qw(Nummer: Dauer: Kosten: Art:)]);
+        $self->mech->get($self->yesss_bookings . "page=$page");
         $table_extract->parse($self->mech->content);
         foreach my $row ($table_extract->rows) {
+            next if $row->[0] =~ /datum/i;
             my $date = substr($row->[0],0,10);
-            last PAGE if $date eq $last_day_of_prev_month; 
-            say join(';',$date,$row->[3],$row->[4]) if $self->verbose;
-            $sum+=$row->[3];
+            last PAGE if $date eq $last_day_of_prev_month;
+            say join(';',$date,$row->[4],$row->[5]) if $self->verbose;
+            $sum+=$row->[4];
+            $types{$row->[5]}++;
         }
     }  
     
@@ -65,15 +73,17 @@ sub get_this_months_bookings {
         my $now = DateTime->now->truncate('to'=>'day');
         my $days = DateTime->last_day_of_month(year=>$now->year,month=>$now->month)->day || 30;
         my $soll = sprintf("%5.3f",($budget/$days) * $now->day);
-        say "$sum / $soll";
+        say "Ausgegeben: $sum Euro";
+        say "Budget bis heute: $soll Euro";
         my $rest = $soll - $sum;
         if ($rest > 0) {
-            my $sms_left = int($rest / 0.068);
-            say "Noch $sms_left SMS möglich";
+            my $sms_left = int($rest / 0.04);
+            say "Noch $sms_left SMS/Telefonate möglich";
         }
         else {
             say "STOP! Du bis drüber!";
         }
+        say join(', ',map { "$_: ".$types{$_} } keys %types);
     }
     else {
         say $sum;
